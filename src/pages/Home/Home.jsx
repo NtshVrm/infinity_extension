783e0f957d031cd74300fc2be5363bf1043d788c
NtshVrm <nitishvarma2k@gmail.com>
2022-03-31 22:58:16 +0530

    feat: button on input change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef, useState } from "react";
 import "./home.css";
 
 export default function Home() {
+  const nameInput = useRef("");
+  const [userName, setUserName] = useState("");
+
   return (
     <div className="home-container banner">
       <div className="content">
         <p className="intro-text">Hey there! What's your name?</p>
-        <input type="text" className="intro-input" />
+        <input
+          type="text"
+          className="intro-input"
+          ref={nameInput}
+          onChange={() => setUserName(nameInput.current.value)}
+        />
       </div>
+
+      {userName !== "" ? (
+        <button className="intro-button">
+          <div className="icon">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          Get Started
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
